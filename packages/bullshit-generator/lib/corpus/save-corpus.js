79becefd57804d467714d87f83be383962aef761
plasticine <975036719@qq.com>
2022-11-04 23:04:54 +0800

    feat: add timestamp to generated corpus filename

--- packages/bullshit-generator/lib/corpus/save-corpus.js
+++ packages/bullshit-generator/lib/corpus/save-corpus.js
@@ -1,6 +1,8 @@
 import { access, mkdir, writeFile } from 'fs/promises'
 import { resolve } from 'path'
 
+import moment from 'moment'
+
 import { ROOT_PATH } from './constants.js'
 
 /**
@@ -9,8 +11,9 @@ import { ROOT_PATH } from './constants.js'
  * @param {Array<string>} article 文章段落数组
  */
 async function saveCorpus(title, article) {
+  const time = moment().format('-YYYY-MM-DD-HH:mm:ss')
   const outputDir = resolve(ROOT_PATH, 'output')
-  const outputFilePath = resolve(outputDir, `${title}.txt`)
+  const outputFilePath = resolve(outputDir, `${title}${time}.txt`)
 
   // 保证 output 目录存在
   try {
