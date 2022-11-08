098d87edd6213ddda1b895f5d66330ef3bf02d4c
plasticine <975036719@qq.com>
2022-11-08 11:23:24 +0800

    refactor(bullshit-generator: interact command): replace process.stdin.on with readline

--- packages/bullshit-generator/lib/interact/index.js
+++ packages/bullshit-generator/lib/interact/index.js
@@ -1,48 +1,33 @@
+import readline from 'readline'
+
 /**
  * @description 交互式生成废话文章
  * @param { Array<{question: string, defaultValue: string | number}> } questionList 问题对象数组
  */
-function interact(questionList) {
-  // 记录原来的编码格式
-  const originalReadableEncoding = process.stdin.readableEncoding
-
-  // 设置成 utf-8 以支持中文
-  process.stdin.setEncoding('utf-8')
-
-  return new Promise(resolve => {
-    /**
-     * @description 用户输入的关于 questionList 的回答
-     * @type { string[] }
-     */
-    const inputAnswers = []
-
-    // 记录当前处理到第几个问题
-    let questionIdx = 0
-
-    let { question, defaultValue } = questionList.at(questionIdx++)
-
-    if (question) {
-      console.log(`${question}(默认值: ${defaultValue})`)
-
-      process.stdin.on('readable', () => {
-        const chunk = process.stdin.read().slice(0, -1)
-
-        inputAnswers.push(chunk || defaultValue)
-
-        // 继续处理下一个问题
-        const nextQuestion = questionList.at(questionIdx++)
-        if (nextQuestion) {
-          process.stdin.read()
-          question = nextQuestion.question
-          defaultValue = nextQuestion.defaultValue
-          console.log(`${question}(默认值: ${defaultValue})`)
-        } else {
-          process.stdin.setEncoding(originalReadableEncoding)
-          resolve(inputAnswers)
-        }
-      })
-    }
+async function interact(questionList) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   })
+
+  const questionPromise = ({ question, defaultValue }) => {
+    const query = `${question}(默认值: ${defaultValue})\n`
+    return new Promise(resolve => {
+      rl.question(query, answer => {
+        resolve(answer || defaultValue)
+      })
+    })
+  }
+
+  const answers = []
+  for (const question of questionList) {
+    const answer = await questionPromise(question)
+    answers.push(answer)
+  }
+
+  rl.close()
+
+  return answers
 }
 
 export { interact }
